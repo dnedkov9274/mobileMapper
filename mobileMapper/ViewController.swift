830ff7610e65e8f400a2ac70cf458689026635a5
Dennis Nedkov <Dnedkov9274@jh-mba-jacks.local>
2019-03-07 11:22:21 -0600

    added Location services, able to track location, default is Cupertino, CA

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -11,10 +11,12 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
 
 
