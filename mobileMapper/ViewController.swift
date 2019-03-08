2d89fb82f299714bb3b842ccc5969419517de81b
Dennis Nedkov <Dnedkov9274@jh-mba-jacks.local>
2019-03-08 11:05:47 -0600

    added BarButton items, as well as 2 IBActions

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate{
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
-    
+    var currentLocation: CLLocation!
     override func viewDidLoad() {
         super.viewDidLoad()
        locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+        print(currentLocation)
+    }
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+    }
+    
 }
 
